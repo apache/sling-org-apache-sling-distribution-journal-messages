87f4075b0b557870e257edf60a462448124a6389
josec <josec@adobe.com>
2022-09-09 12:43:01 +0200

    Fix

--- src/main/java/org/apache/sling/distribution/journal/package-info.java
+++ src/main/java/org/apache/sling/distribution/journal/package-info.java
@@ -16,6 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-@org.osgi.annotation.versioning.Version("3.0.0")
+@org.osgi.annotation.versioning.Version("3.1.0")
 package org.apache.sling.distribution.journal;
 
